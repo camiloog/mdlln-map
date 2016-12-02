489c6e19ec1c9fe5568250049dfe09e3f375f388
Camilo Ocampo <camiloog@users.noreply.github.com>
2016-12-02 02:38:25 -0500

    change dummy map for real map

--- app/scripts/leaflet-src.js
+++ app/scripts/leaflet-src.js
@@ -4,7 +4,7 @@
 */
 (function (window, document, undefined) {
 var L = {
-	version: "1.0.2"
+	version: '1.0.2'
 };
 
 function expose() {
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -6,26 +6,26 @@ $('#menu-toggle').click(function(e) {
 $('#wrapper').toggleClass('toggled');
 
 // map scripts
-var map = L.map('map').setView([37.8, -96], 4);
-L.geoJson(statesData).addTo(map);
-function getColor(d) {
-    return d > 1000 ? '#800026' :
-           d > 500  ? '#BD0026' :
-           d > 200  ? '#E31A1C' :
-           d > 100  ? '#FC4E2A' :
-           d > 50   ? '#FD8D3C' :
-           d > 20   ? '#FEB24C' :
-           d > 10   ? '#FED976' :
-                      '#FFEDA0';
-}
-function style(feature) {
-    return {
-        fillColor: getColor(feature.properties.density),
-        weight: 2,
-        opacity: 1,
-        color: 'white',
-        dashArray: '3',
-        fillOpacity: 0.7
-    };
-}
-L.geoJson(statesData, {style: style}).addTo(map);
+var map = L.map('map').setView([6.2691, -75.5956], 12);
+L.geoJson(comcorr).addTo(map);
+// function getColor(d) {
+//     return d > 1000 ? '#800026' :
+//            d > 500  ? '#BD0026' :
+//            d > 200  ? '#E31A1C' :
+//            d > 100  ? '#FC4E2A' :
+//            d > 50   ? '#FD8D3C' :
+//            d > 20   ? '#FEB24C' :
+//            d > 10   ? '#FED976' :
+//                       '#FFEDA0';
+// }
+// function style(feature) {
+//     return {
+//         fillColor: getColor(feature.properties.density),
+//         weight: 2,
+//         opacity: 1,
+//         color: 'white',
+//         dashArray: '3',
+//         fillOpacity: 0.7
+//     };
+// }
+// L.geoJson(statesData, {style: style}).addTo(map);
