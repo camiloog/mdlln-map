e54a0d544e11ea8b97a86a13534bdecf766fde64
Camilo Ocampo <camiloog@users.noreply.github.com>
2016-12-01 23:08:20 -0500

    added simple sidebar

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,8 @@
-console.log('\'Allo \'Allo!');
+// Togle function for sidebar
+$("#menu-toggle").click(function(e) {
+    e.preventDefault();
+    $("#wrapper").toggleClass("toggled");
+});
+$("#wrapper").toggleClass("toggled");
+
+var L = require('leaflet');
