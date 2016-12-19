a9a69e43ddc7e7e4b30f752e929c89995d99e328
Camilo Ocampo <camiloog@users.noreply.github.com>
2016-12-18 21:52:33 -0500

    Typo fixed.

--- app/scripts/resources.js
+++ app/scripts/resources.js
@@ -2,12 +2,12 @@ var r_data = {
   'NONE': [
     {'USAGE':'Seleccione un recurso natural del menú para ver los mapas relacionados. Se asocia a cada recurso un icono para su fácil identificación.','GIF':'./tutorial/click_recurso.gif'},
     {'USAGE':'Active o desactive mapas desde el boton de capas.','GIF':'./tutorial/seleccionar_capas.gif'},
-    {'USAGE':'Use el la rueda del mouse y las teclas <span style="border: 1px solid;padding: 0 2px;border-radius: 2px;" title="Más">+</span> y <span style="border: 1px solid;padding: 0 3px;border-radius: 2px;" title="Menos">-</span> para hacer zoom en el mapa.','GIF':'./tutorial/zoom.gif'},
+    {'USAGE':'Use el la rueda del mouse o las teclas <span style="border: 1px solid;padding: 0 2px;border-radius: 2px;" title="Más">+</span> y <span style="border: 1px solid;padding: 0 3px;border-radius: 2px;" title="Menos">-</span> para hacer zoom en el mapa.','GIF':'./tutorial/zoom.gif'},
     {'USAGE':'Haga click sostenido sobre el mapa y arrastre o use las teclas de dirección para navegar por el mapa.','GIF':'./tutorial/click_arrastrar.gif'},
     {'USAGE':'Pase el puntero del mouse sobre los elementos del mapa para visualizar su nombre. De click sobre cualquier elemento para fijar su nombre en la posición deseada.','GIF':'./tutorial/nombres_cursor.gif'},
     {'USAGE':'Ingrese en la vista de comuna o corregimiento haciendo doble click sobre la comuna deseada. Haga doble click nuevamente sobre el mapa para salir de esta vista.','GIF':'./tutorial/ingresar_comuna.gif'},
     {'USAGE':'Observe el cuadro de información en la vista de comuna o corregimiento. En este se muestran los iconos de los recursos con los que esta comuna tiene problematicas ambientales. De click en estos iconos para cambiar a la vista del recurso.','GIF':'./tutorial/informacion_comuna.gif'},
-    {'USAGE':'Si necesita más espacio para visualizar el mapa, sobretodo si accede desde un dispositivo mobil, puede colapsar las convenciones del mapa haciendo click sobre ellas. Haga click sobre el boton que aparece en su lugar para visualizarlas nuevamente.','GIF':'./tutorial/colapsar_leyendas.gif'},
+    {'USAGE':'Si necesita más espacio para visualizar el mapa, sobretodo al acceder desde un dispositivo mobil, puede colapsar las convenciones del mapa haciendo click sobre ellas. Haga click sobre el boton que aparece en su lugar para visualizarlas nuevamente.','GIF':'./tutorial/colapsar_leyendas.gif'},
     {'USAGE':'Regrese a este manual y limpie el mapa dando click en el boton Limpiar del menú principal.'}
   ],
   'REC_AGUA': [
